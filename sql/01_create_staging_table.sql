5be7d838991dfd2b3e3ccb89bca32e4d6854e50e
Yawndu <xadrianpinera@gmail.com>
2026-09-04 17:27:51 +0800

    Delete sql directory

--- sql/01_create_staging_table.sql
+++
@@ -1,3 +0,0 @@
-CREATE table veterans_disability (fips_code TEXT, state TEXT, county_name TEXT, total_recipients TEXT, 
-scd_0_20 TEXT, scd_30_40 TEXT, scd_50_60 TEXT, scd_70_90 TEXT, scd_100 TEXT, age_17_44 TEXT, age_45_64 TEXT, 
-age_65_older TEXT, male TEXT, female TEXT);
