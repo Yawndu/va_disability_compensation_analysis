855b401e6247c40cec3ad0310871f7a35c40664a
Yawndu <xadrianpinera@gmail.com>
2026-09-04 17:22:26 +0800

    Delete 05_validation_checks.sql

--- 05_validation_checks.sql
+++
@@ -1,6 +0,0 @@
-SELECT * from veterans_disability
-where county_name = 'Jefferson' AND state = 'Alabama';
-
-select count (*) from veterans_disability;
-select count (*) from veterans_disability_other;
-select count (*) from staging_va;
