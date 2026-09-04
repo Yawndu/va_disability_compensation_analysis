bffaef9cf335e52a92da7f01da0f9835e5dc2b7e
Yawndu <xadrianpinera@gmail.com>
2026-09-04 17:22:16 +0800

    Delete 04_separate_non_counties.sql

--- 04_separate_non_counties.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE veterans_disability_other as 
-SELECT * FROM veterans_disability 
-WHERE county_name IN ('Unknwon', 'US Territories (excluding Puerto Rico)', 'Other Foreign Countries') 
-	OR fips_code IN ('00000', '88888', 'Unknown', '72000');
-
-DELETE FROM veterans_disability 
-WHERE county_name IN ('Unknwon', 'US Territories (excluding Puerto Rico)', 'Other Foreign Countries') 
-	OR fips_code IN ('00000', '88888', 'Unknown', '72000');
